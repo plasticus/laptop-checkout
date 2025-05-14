7fa18deffa566d8762fa0f03e177cc19b5c54edb
Marquardt <marquarc@proton.me>
2025-05-14 16:41:51 -0500

    update nav

--- nav.php
+++ nav.php
@@ -1,10 +1,11 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
 <div class="navbar">
     <img src="isea.png" alt="ISEA Logo" class="logo">
     <nav>
-        <a href="index.php">Home</a>
-        <a href="equipment.php">Equipment</a>
-        <a href="staff.php">Staff</a>
-        <a href="checkout.php">Checkout</a>
-        <a href="reports.php">Reports</a>
+        <a href="index.php"><i class="fas fa-home"></i> Home</a>
+        <a href="equipment.php"><i class="fas fa-laptop"></i> Equipment</a>
+        <a href="checkout.php"><i class="fas fa-arrow-right-to-bracket"></i> Checkout</a>
+        <a href="reports.php"><i class="fas fa-chart-bar"></i> Reports</a>
     </nav>
 </div>
